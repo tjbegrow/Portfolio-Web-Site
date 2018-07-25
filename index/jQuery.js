ff28445f4aa65d38a4fd8856896dc85799ec0efb
tjbegrow <41201449+tjbegrow@users.noreply.github.com>
2018-07-25 12:42:10 -0500

    Menu Perfected with CSS @Media
    Nav labels no longer show on small screens. No jQuery required.

--- index/jQuery.js
+++ index/jQuery.js
@@ -1,33 +1,26 @@
 $(document).ready(function(){
-    /*window.addEventListener("resize", showTitles);*/
-    function showTitles() {
-        var width = $(window).width();
-        if (width > 800) {
-            $("#home").mouseenter(function(){
-                $("#homeTitle").show();
-            });
-            $("#home").mouseleave(function(){
-                $("#homeTitle").hide();
-            });
-            $("#search").mouseenter(function(){
-                $("#searchTitle").show();
-            });
-            $("#search").mouseleave(function(){
-                $("#searchTitle").hide();
-            });
-            $("#code").mouseenter(function(){
-                $("#codeTitle").show();
-            });
-            $("#code").mouseleave(function(){
-                $("#codeTitle").hide();
-            });
-            $("#contact").mouseenter(function(){
-                $("#contactTitle").show();
-            });
-            $("#contact").mouseleave(function(){
-                $("#contactTitle").hide();
-            });
-        }
-    }
-    showTitles();
+    $("#home").mouseenter(function(){
+        $("#homeTitle").show();
+    });
+    $("#home").mouseleave(function(){
+        $("#homeTitle").hide();
+    });
+    $("#search").mouseenter(function(){
+        $("#searchTitle").show();
+    });
+    $("#search").mouseleave(function(){
+        $("#searchTitle").hide();
+    });
+    $("#code").mouseenter(function(){
+        $("#codeTitle").show();
+    });
+    $("#code").mouseleave(function(){
+        $("#codeTitle").hide();
+    });
+    $("#contact").mouseenter(function(){
+        $("#contactTitle").show();
+    });
+    $("#contact").mouseleave(function(){
+        $("#contactTitle").hide();
+    });
 });
