9e6c61259ce99158c1775ae0c1551511cd280cd2
tjbegrow <tjbegrow@gmail.com>
2018-07-25 08:05:49 -0500

    Nav Bar jQuery half done

--- index/jQuery.js
+++ index/jQuery.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
-    $("#home").mouseenter(function(){
-        $("#homeTitle").show();
-    });
-    $("#home").mouseleave(function(){
-        $("#homeTitle").hide();
-    });
-    $("#search").mouseenter(function(){
-        $("#searchTitle").show();
-    });
-    $("#search").mouseleave(function(){
-        $("#searchTitle").hide();
-    });
-    $("#code").mouseenter(function(){
-        $("#codeTitle").show();
-    });
-    $("#code").mouseleave(function(){
-        $("#codeTitle").hide();
-    });
-    $("#contact").mouseenter(function(){
-        $("#contactTitle").show();
-    });
-    $("#contact").mouseleave(function(){
-        $("#contactTitle").hide();
-    });
+    /*window.addEventListener("resize", showTitles);*/
+    function showTitles() {
+        var width = $(window).width();
+        if (width > 800) {
+            $("#home").mouseenter(function(){
+                $("#homeTitle").show();
+            });
+            $("#home").mouseleave(function(){
+                $("#homeTitle").hide();
+            });
+            $("#search").mouseenter(function(){
+                $("#searchTitle").show();
+            });
+            $("#search").mouseleave(function(){
+                $("#searchTitle").hide();
+            });
+            $("#code").mouseenter(function(){
+                $("#codeTitle").show();
+            });
+            $("#code").mouseleave(function(){
+                $("#codeTitle").hide();
+            });
+            $("#contact").mouseenter(function(){
+                $("#contactTitle").show();
+            });
+            $("#contact").mouseleave(function(){
+                $("#contactTitle").hide();
+            });
+        }
+    }
+    showTitles();
 });
